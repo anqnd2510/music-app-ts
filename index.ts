7ebd9779b14e77a68f22a4ea73ad9608bfbe29fa
anqnd2510 <duyan251003@gmail.com>
2023-12-08 16:41:20 +0700

    Embedded static file

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
 

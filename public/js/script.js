c529d30b78d6ced4aac44720c623f9107e3d5558
anqnd2510 <duyan251003@gmail.com>
2023-12-11 17:53:08 +0700

    tính năng like

--- public/js/script.js
+++ public/js/script.js
@@ -26,10 +26,37 @@ if(aplayer) {
 
     ap.on('play', function () {
         avatar.style.animationPlayState = "running";
-      });
+    });
 
     ap.on('pause', function () {
         avatar.style.animationPlayState = "paused";
-      });
+    });
 }
 // End APlayer
+
+// Button Like
+const buttonLike = document.querySelector("[button-like]");
+if(buttonLike) {
+    buttonLike.addEventListener("click", () => {
+        const idSong = buttonLike.getAttribute("button-like");
+        const isActive = buttonLike.classList.contains("active");
+
+        const typeLike = isActive ? "dislike" : "like";
+
+        const link = `/songs/like/${typeLike}/${idSong}`;
+
+        const option = {
+            method: "PATCH"
+        }
+
+        fetch (link, option) 
+        .then(res => res.json())
+        .then( data => {
+            const span = buttonLike.querySelector("span");
+            span.innerHTML = `${data.like} thích`;
+
+            buttonLike.classList.toggle("active");
+        });
+    });
+}
+// End Button Like

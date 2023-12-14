18f7ca0c62c8a9932571c79d7c6398e6a2ea3dd5
anqnd2510 <duyan251003@gmail.com>
2023-12-14 14:46:39 +0700

    Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -13,3 +13,22 @@ if(uploadImage) {
     });
 }
 // End Upload and Preview Image
+
+
+// Upload and Preview Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if(uploadAudio) {
+    const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+    const uploadAudioPreview = uploadAudio.querySelector("[upload-audio-preview]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        if (e.target.files.length) {
+            const audio = URL.createObjectURL(e.target.files[0]);
+
+            source.src = audio;
+            uploadAudioPreview.load();
+        }
+    });
+}
+// End Upload and Preview Audio

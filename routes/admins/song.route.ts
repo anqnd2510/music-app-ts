985d2b1793fc00fe452023057295326160fe46ff
anqnd2510 <duyan251003@gmail.com>
2023-12-14 11:57:01 +0700

    Làm giao diện cho thêm mới bài hát

--- routes/admins/song.route.ts
+++ routes/admins/song.route.ts
@@ -5,4 +5,6 @@ import * as controller from "../../controllers/admins/song.controller";
 
 router.get("/", controller.index);
 
+router.get("/create", controller.create);
+
 export const songRoutes: Router = router;
